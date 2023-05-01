072476ce55d1a246dce683938e6433f59043c6b6
ATUL TINGRE <atultingre.work@gmail.com>
2023-05-01 22:19:50 +0530

    Redux Todo App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { AddTodoAction, RemoveTodoAction } from "./store/actions/TodoActions";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 function App() {
+  const [todo, setTodo] = useState();
+  const dispatch = useDispatch();
+
+  const Todo = useSelector((state) => state.Todo);
+  const { todos } = Todo;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddTodoAction(todo));
+  };
+
+  const removeTodo = (t) => {
+    dispatch(RemoveTodoAction(t));
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Todo List</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter a Todo"
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit" className="btn-add">
+            Add
+          </button>
+        </form>
+        <ul className="allTodo">
+          {todos &&
+            todos.map((t) => (
+              <li key={t.id} className="singleTodo">
+                <span className="todo-text">{t.todo}</span>
+                <button className="btn-del" onClick={() => removeTodo(t)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+        </ul>
       </header>
     </div>
   );
---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { TodoReducer } from "./reducers/TodoReducers";
+
+const reducer = combineReducers({
+    Todo: TodoReducer
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+ const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store;
